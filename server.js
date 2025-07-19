49909abf955a06a3e1545ac9e7d09264047f4f53
Kaustav Ray <kkray1345@gmail.com>
2025-07-19 18:41:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const dbUris = [
   'mongodb+srv://aohtvv0:bQuM9iRZm91GrPKe@cluster0.qvwhl5x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
   'mongodb+srv://hb6g6vff6gf:Gyjp3wBB8FGyqpXz@cluster0.jkbqe5x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
   'mongodb+srv://aq9d7qh:fKjI8toWE6kVixkL@cluster0.umimpv3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-  'mongodb+srv://tgonjm3m:ExkshiAvko74PU35@cluster0.eg776s0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+  'mongodb+srv://tf4hz7z:TXAJQlbquHbNUK2N@cluster0.0qfgeqz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 ];
 
 let clients = [], buckets = [];
